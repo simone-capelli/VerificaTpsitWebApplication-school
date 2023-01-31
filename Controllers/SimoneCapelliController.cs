1709ae071409c73b7dfed690ae0dfb9a6b93c304
simo-sw <simonecapelli13@gmail.com>
2023-01-31 13:35:34 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/SimoneCapelliController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebAppMvcSimoneCapelli.Models;
+
+namespace WebAppMvcSimoneCapelli.Controllers
+{
+    public class SimoneCapelliController : Controller
+    {
+        private readonly ILogger<SimoneCapelliController> _logger;
+
+        public SimoneCapelliController(ILogger<SimoneCapelliController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult SimoneCapelliPage(string name="", string msg="", int numRipetizioni=0)
+        {
+            try
+            {
+                ViewData["name"] = name;
+                _logger.LogInformation("Questo è un information log");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Questo è un errore: " + ex.Message);
+            }
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppMvcSimoneCapelli.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
